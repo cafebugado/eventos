d2c9805aee4e1fbf61fc9dfdc296c6427b765696
Lírio <stilldenis@outlook.com>
2026-02-20 16:08:38 -0300

    feat(favoritar-eventos): correção das cores em light e dark mode

--- src/components/favourite-event/favouriteEventButton.jsx
+++ src/components/favourite-event/favouriteEventButton.jsx
@@ -4,44 +4,22 @@ import { memo } from 'react'
 export const FavouriteEventButton = memo(({ event, isFavourite, onToggle, isCard }) => {
   if (!isCard) {
     return (
-      // <div
-      //   className="card-heart-div"
-
-      // >
-      //   <p>Favoritar</p>
-      //   <Heart fill={isFavourite ? 'red' : 'transparent'} color="red" size={24} />
-      // </div>
-
       <button
         onClick={(e) => {
           e.stopPropagation()
           onToggle(event.id)
         }}
-        className="participate-button"
-        style={{
-          backgroundColor: 'transparent',
-          outline: '1px solid white',
-          fontSize: '16px',
-          zIndex: 30,
-        }}
+        className="favourite-button"
       >
         {isFavourite ? 'Remover dos favoritos' : 'Favoritar'}
         <Heart
           fill={isFavourite ? 'red' : 'transparent'}
-          style={{ color: isFavourite ? 'transparent' : 'white' }}
+          style={{ color: isFavourite ? 'transparent' : 'red' }}
         />
       </button>
     )
   } else {
     return (
-      // <div
-      //   className="card-heart-div"
-
-      // >
-      //   <p>Favoritar</p>
-      //   <Heart fill={isFavourite ? 'red' : 'transparent'} color="red" size={24} />
-      // </div>
-
       <button
         onClick={(e) => {
           e.stopPropagation()
@@ -49,10 +27,7 @@ export const FavouriteEventButton = memo(({ event, isFavourite, onToggle, isCard
         }}
         className="card-heart-div"
       >
-        <Heart
-          fill={isFavourite ? 'red' : 'transparent'}
-          style={{ color: isFavourite ? 'transparent' : 'white' }}
-        />
+        <Heart size={24} className={`heart-icon ${isFavourite ? 'is-active' : ''}`} />
       </button>
     )
   }
